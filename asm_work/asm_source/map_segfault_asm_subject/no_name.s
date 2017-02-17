7930ad500e69faf9f672a90a34147dfa39abfa67
Ramzi BOUGUERRA <rabougue@e3r13p13.42.fr>
2017-02-17 16:59:39 +0100

    add segfaultmap

---
+++ asm_work/asm_source/map_segfault_asm_subject/no_name.s
@@ -0,0 +1,26 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    test.s                                             :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: rabougue <marvin@42.fr>                    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2016/12/17 15:56:18 by rabougue          #+#    #+#              #
+#    Updated: 2017/02/17 16:55:58 by rabougue         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+#klkjl
+ ;klkjl	
+#.name	"le iProg_name Le systeme hexadecimal est utilise notamment en electronique numerique et en informatique car il est particulereme"	;kjhkljh
+#kjkjk
+
+.name ""
+.comment "lkjhgfslkhiiiiiiiiiiiiiiiiiiiiiiiiiiiiiij"
+
+abcdefghijklmnopqrstuvwxyz_0123456789:
+
+l2:		sti r1, %:live, %1
+		and r1, %0, r1
+
+live:	live %1
+		zjmp %:live
